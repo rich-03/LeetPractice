945b228b614fb6fe53dbd82621e2fe7b92f299dd
rich-03 <richbel03@icloud.com>
2020-04-15 14:16:10 -0400

    Added explanation of the approach

---
+++ Problem0001.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
+
+[2, 7, 11, 15], target = 9,
+assert(Solution().twoSum([2, 7, 11, 15], 9) == [0, 1])
--- Problem0242.py
+++ Problem0242.py
@@ -1,3 +1,7 @@
+# Approach:
+# two strings are anagrams if their canonical presentations (sorted lists of characters) are identical
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
