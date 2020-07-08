c894815a0b5a45b3b7555dc66fb699de04647d38
rich-03 <richbel03@icloud.com>
2020-07-07 21:18:34 -0400

    Added imperative solution to #0001

--- Problem0001.py
+++ Problem0001.py
@@ -1,8 +1,14 @@
 from typing import List
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        d = dict()
+        for n in enumerate(nums):
+            if target - n[1] in d:
+                return [d[target - n[1]], n[0]]
+            else:
+                d.update({n[1]: n[0]})
 
-[2, 7, 11, 15], target = 9,
-assert(Solution().twoSum([2, 7, 11, 15], 9) == [0, 1])
+assert (Solution().twoSum([2, 7, 11, 15], 9) == [0, 1])
+assert (Solution().twoSum([2, 7, 11, 15, 1], 3) == [0, 4])
