2ba768d18a9612a93b49e0f89a830ef14db89167
rich-03 <richbel03@icloud.com>
2020-07-08 23:35:15 -0400

    Done LeetCode Problem #35

--- Problem0035/Problem0035.py
+++ Problem0035/Problem0035.py
@@ -1,4 +1,9 @@
 from typing import List
 
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        for i, v in enumerate(nums):
+            if v >= target:
+                return i
+        return len(nums)
