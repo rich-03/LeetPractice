ce7a94b28d16e04703983b7e215e8afe5dd5f6bb
rich-03 <richbel03@icloud.com>
2020-07-15 22:58:16 -0400

    Done Leetcode Problem #1470

---
+++ Problem1470/Problem1470.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        shuffle = []
+
+        for i in range(n):
+            shuffle.append(nums[i])
+            shuffle.append(nums[i + n])
+
+        return shuffle
