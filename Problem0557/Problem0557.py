5c609917ce651246cc6c5d848d8be531265b7e91
rich-03 <richbel03@icloud.com>
2020-09-07 12:14:45 -0400

    Done Leetcode Problem #557

---
+++ Problem0557/Problem0557.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.split()
+        for i in range(len(s)):
+            s[i] = s[i][::-1]
+        s = " ".join(s)
+
+        return s
